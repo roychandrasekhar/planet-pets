5f1bdfc8f5dd410a5acb4de994aa1a9fd10d2f18
Chandra Sekhar Roy <roy.chandrasekhar@gmail.com>
2024-01-18 00:07:01 +0530

    Update provisioner.sh

--- phase2/provisioner.sh
+++ phase2/provisioner.sh
@@ -7,8 +7,14 @@ sudo yum -y update
 sudo yum install git -y
 
 # Install Java 17
-sudo amazon-linux-extras enable java-openjdk17
-sudo yum install -y java-1.17.0-openjdk-devel
+sudo yum install -y curl
+curl -O https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_linux-x64_bin.tar.gz
+sudo tar -xzvf openjdk-17_linux-x64_bin.tar.gz -C /opt
+
+echo 'export JAVA_HOME=/opt/jdk-17' | sudo tee -a /etc/profile.d/java.sh
+echo 'export PATH=$JAVA_HOME/bin:$PATH' | sudo tee -a /etc/profile.d/java.sh
+sudo chmod +x /etc/profile.d/java.sh
+source /etc/profile.d/java.sh
 
 git clone https://github.com/spring-projects/spring-petclinic.git
 cd spring-petclinic
